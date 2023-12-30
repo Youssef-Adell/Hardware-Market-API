545ab5f8b32bfe6e6fe733f4f5357775c1b53d06
YoussefAdel <al3nkpout2@gmail.com>
2023-12-30 23:08:24 +0200

    Add Validation to the MaxPrice query parameter to ensure that it is greater than MinPrice in Get Products endpoint

--- Core/DTOs/SpecificationDTOs/ProductsSpecificationParameters.cs
+++ Core/DTOs/SpecificationDTOs/ProductsSpecificationParameters.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Core.DTOs.SpecificationDTOs;
 
-public class ProductsSpecificationParameters : SpecificationParameters
+public class ProductsSpecificationParameters : SpecificationParameters, IValidatableObject
 {
     public String? Search { get; set; }
     public uint MinPrice { get; set; }
     public uint MaxPrice { get; set; } = int.MaxValue;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (MaxPrice <= MinPrice)
+            yield return new ValidationResult("MaxPrice should be greater than MinPrice.");
+    }
+
 }
